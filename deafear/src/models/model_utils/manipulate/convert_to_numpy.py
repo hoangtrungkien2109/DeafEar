417a9bef6be05e507ac31fc644424d543ed4841e
TrH203 <hoangtrhien203@gmail.com>
2024-11-03 16:03:10 +0700

    Update: 2 file convert to numpy and vd

--- deafear/src/models/model_utils/manipulate/convert_to_numpy.py
+++ deafear/src/models/model_utils/manipulate/convert_to_numpy.py
@@ -1,59 +1,98 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import glob
+import os
+import logging
+import colorlog
 
-# Initialize mediapipe holistic model
+def file_exists(folder_path, file_name):
+    file_name = f"landmarks_{file_name}.npy"
+    file_path = os.path.join(folder_path, file_name)
+    return os.path.isfile(file_path)
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='convert_data.log',level=logging.DEBUG)
+
+video_folder = '/Users/trHien/PycharmProjects/ScrapeASLData/data'
+destiny_folder = "./data_processed"
+
 mp_holistic = mp.solutions.holistic
 
-video_file = '/Users/trHien/PycharmProjects/ScrapeASLData/data/D0013.mp4'  # Replace with your video path
-cap = cv2.VideoCapture(video_file)
+video_files = glob.glob(os.path.join(video_folder, '*[0-9].mp4'))[:200]
 
-landmarks_data = []
+for video_file in video_files:
+    file_name_with_ext = os.path.basename(video_file)
+    
+    file_name = os.path.splitext(file_name_with_ext)[0]
+    if file_exists(destiny_folder,file_name):
+        logger.warning(f"{file_name} is existed - Continue")
+        continue 
 
-with mp_holistic.Holistic(static_image_mode=False, model_complexity=1, enable_segmentation=False, min_detection_confidence=0.1) as holistic:
-    while cap.isOpened():
-        ret, frame = cap.read()
+    cap = cv2.VideoCapture(video_file)
 
-        if not ret:
-            print("End of video or cannot read the video stream.")
-            break
+    # Labeling
+    frame_idx = 0
+    labels = []
+    landmarks_data = []
+    
+    with mp_holistic.Holistic(static_image_mode=False, model_complexity=2, enable_segmentation=False, min_detection_confidence=0.5) as holistic:
+        while cap.isOpened():            
+            ret, frame = cap.read()
+            
+            if not ret:
+                print("End of video or cannot read the video stream.")
+                break
 
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
+            # cv2.imshow('Video Frame', frame)
+            
+            
+            frame_idx += 1
 
-        results = holistic.process(image)
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
 
-        frame_data = np.zeros((75, 3))  # 75 keypoints (33 pose, 21 right-hand, 21 left-hand), with 3 coordinates each (x, y, z)
+            results = holistic.process(image)
 
-        # Collect key points from the pose, hands (left and right)
-        if results.pose_landmarks:
-            for idx, lm in enumerate(results.pose_landmarks.landmark):
-                frame_data[idx] = [lm.x, lm.y, lm.z]
+            frame_data = np.zeros((75, 3))  # 75 keypoints (33 pose, 21 right-hand, 21 left-hand), with 3 coordinates each (x, y, z)
+            
+            # Collect key points from the pose, hands (left and right)
+            if results.pose_landmarks:
+                for idx, lm in enumerate(results.pose_landmarks.landmark):
+                    frame_data[idx] = [lm.x, lm.y, lm.z]
 
-        # Check for right hand landmarks and append only if they exist
-        if results.right_hand_landmarks:
-            for idx, lm in enumerate(results.right_hand_landmarks.landmark):
-                frame_data[33 + idx] = [lm.x, lm.y, lm.z]  # Right hand starts after 33 pose landmarks
-        else:
-            # Ignore the right hand if not present
-            frame_data[33:33 + 21] = np.nan  # Optionally mark absent hand landmarks with NaN
+            # Check for right hand landmarks and append only if they exist
+            if results.right_hand_landmarks:
+                for idx, lm in enumerate(results.right_hand_landmarks.landmark):
+                    frame_data[33 + idx] = [lm.x, lm.y, lm.z]  # Right hand starts after 33 pose landmarks
+            else:
+                frame_data[33:33 + 21] = 0  # Mark absent hand landmarks with 0
 
-        # Check for left hand landmarks and append only if they exist
-        if results.left_hand_landmarks:
-            for idx, lm in enumerate(results.left_hand_landmarks.landmark):
-                frame_data[33 + 21 + idx] = [lm.x, lm.y, lm.z]  # Left hand starts after 33 pose + 21 right hand landmarks
-        else:
-            # Ignore the left hand if not present
-            frame_data[33 + 21:33 + 42] = np.nan  # Optionally mark absent hand landmarks with NaN
 
-        landmarks_data.append(frame_data)
+            # Check for left hand landmarks and append only if they exist
+            if results.left_hand_landmarks:
+                for idx, lm in enumerate(results.left_hand_landmarks.landmark):
+                    frame_data[33 + 21 + idx] = [lm.x, lm.y, lm.z]  # Left hand starts after 33 pose + 21 right hand landmarks
+            else:
+                frame_data[33 + 21:33 + 42] = 0  # Mark absent hand landmarks with 0
 
-landmarks_array = np.array(landmarks_data)
-print(f"Shape: {landmarks_array.shape}")
-# Save to a npy file
-np.save('landmarks_data4.npy', landmarks_array)
+            landmarks_data.append(frame_data)
 
-print("Landmarks data saved to landmarks_data.npy")
 
-# Release resources
-cap.release()
+    # Convert data to numpy arrays
+    landmarks_array = np.array(landmarks_data)
+    # labels_array = np.array(labels)
+
+    # print(f"Shape landmarks: {landmarks_array.shape}")
+    # print(f"Shape labels: {labels_array.shape}")
+
+    # Save to npy file
+    np.save(destiny_folder + f'/landmarks_{file_name}.npy', landmarks_array)
+    # np.save(f'./temp/labels_{file_name}.npy', labels_array)
+    # print("Error:",error)
+    logger.info(f"Data in {file_name} saved to landmarks_{file_name}.npy")
+
+    # Release resources
+    cap.release()
+    
+cv2.destroyAllWindows()
--- deafear/src/models/model_utils/manipulate/convert_to_vd.py
+++ deafear/src/models/model_utils/manipulate/convert_to_vd.py
@@ -1,17 +1,44 @@
 import cv2
 import numpy as np
-import mediapipe as mp
+from model import load_model, predict
+# Correct connections for pose landmarks in MediaPipe (total 33 landmarks)
+POSE_CONNECTIONS = [
+    (0, 1), (1, 2), (2, 3), (3, 7),          # Right eye to ear
+    (0, 4), (4, 5), (5, 6), (6, 8),          # Left eye to ear
+    (9, 10), (11, 12),                       # Shoulders
+    (11, 13), (13, 15), (15, 17),            # Left arm
+    (12, 14), (14, 16), (16, 18),            # Right arm
+    (23, 24),                                # Hips
+    (24, 26), (26, 28), (28, 32),            # Right leg
+    (23, 25), (25, 27), (27, 29), (29, 31),  # Left leg
+]
 
-mp_drawing = mp.solutions.drawing_utils
-mp_holistic = mp.solutions.holistic
+HAND_CONNECTIONS = [
+    (0, 1), (1, 2), (2, 3), (3, 4),  # Thumb
+    (5, 6), (6, 7), (7, 8),          # Index finger
+    (9, 10), (10, 11), (11, 12),     # Middle finger
+    (13, 14), (14, 15), (15, 16),    # Ring finger
+    (17, 18), (18, 19), (19, 20)     # Pinky finger
+]
 
-def draw_landmarks(image, frame_landmarks):
+def draw_landmarks(image, frame_landmarks,line_thickness=2):
     pose_landmarks = frame_landmarks[:33]
     for lm in pose_landmarks:
         if not np.isnan(lm[0]) and not np.isnan(lm[1]) and not np.isnan(lm[2]):  # Check for NaN values
             x, y = int(lm[0] * image.shape[1]), int(lm[1] * image.shape[0])
             cv2.circle(image, (x, y), 4, (0, 255, 0), -1)
+            
+    # Draw lines between connected pose landmarks
+    for connection in POSE_CONNECTIONS:
+        start_idx, end_idx = connection
+        start_point = pose_landmarks[start_idx]
+        end_point = pose_landmarks[end_idx]
+        if (start_point[0] != 0 and start_point[1] != 0) and (end_point[0] != 0 and end_point[1] != 0):
+            start_x, start_y = int(start_point[0] * image.shape[1]), int(start_point[1] * image.shape[0])
+            end_x, end_y = int(end_point[0] * image.shape[1]), int(end_point[1] * image.shape[0])
+            cv2.line(image, (start_x, start_y), (end_x, end_y), (0, 255, 0), line_thickness)
 
+            
     right_hand_landmarks = frame_landmarks[33:33 + 21]
     right_hand_present = False
     for lm in right_hand_landmarks:
@@ -19,7 +46,17 @@ def draw_landmarks(image, frame_landmarks):
             x, y = int(lm[0] * image.shape[1]), int(lm[1] * image.shape[0])
             cv2.circle(image, (x, y), 4, (255, 0, 0), -1)
             right_hand_present = True
-
+            
+    # Draw lines between connected right hand landmarks
+    for connection in HAND_CONNECTIONS:
+        start_idx, end_idx = connection
+        start_point = right_hand_landmarks[start_idx]
+        end_point = right_hand_landmarks[end_idx]
+        if (start_point[0] != 0 and start_point[1] != 0) and (end_point[0] != 0 and end_point[1] != 0):
+            start_x, start_y = int(start_point[0] * image.shape[1]), int(start_point[1] * image.shape[0])
+            end_x, end_y = int(end_point[0] * image.shape[1]), int(end_point[1] * image.shape[0])
+            cv2.line(image, (start_x, start_y), (end_x, end_y), (255, 0, 0), line_thickness)
+            
     left_hand_landmarks = frame_landmarks[33 + 21:]
     left_hand_present = False
     for lm in left_hand_landmarks:
@@ -27,7 +64,17 @@ def draw_landmarks(image, frame_landmarks):
             x, y = int(lm[0] * image.shape[1]), int(lm[1] * image.shape[0])
             cv2.circle(image, (x, y), 4, (0, 0, 255), -1)
             left_hand_present = True
-
+            
+    # Draw lines between connected left hand landmarks
+    for connection in HAND_CONNECTIONS:
+        start_idx, end_idx = connection
+        start_point = left_hand_landmarks[start_idx]
+        end_point = left_hand_landmarks[end_idx]
+        if (start_point[0] != 0 and start_point[1] != 0) and (end_point[0] != 0 and end_point[1] != 0):
+            start_x, start_y = int(start_point[0] * image.shape[1]), int(start_point[1] * image.shape[0])
+            end_x, end_y = int(end_point[0] * image.shape[1]), int(end_point[1] * image.shape[0])
+            cv2.line(image, (start_x, start_y), (end_x, end_y), (0, 0, 255), line_thickness)
+             
     if not right_hand_present and not left_hand_present:
         return None  # Return None if no hands are detected
 
@@ -35,46 +82,83 @@ def draw_landmarks(image, frame_landmarks):
 
 
 
-def load_and_concatenate_npy_files(npy_files):
+def load_and_concatenate_npy_files(model,npy_files):
     all_landmarks = []
+    prediction = []
     for npy_file in npy_files:
         landmarks_data = np.load(npy_file)
+        
         all_landmarks.append(landmarks_data)
 
-    concatenated_landmarks = np.concatenate(all_landmarks, axis=0)
-    return concatenated_landmarks
+        p = predict(model,landmarks_data)
+        prediction.append(p)
 
-def is_similar_frame(frame1, frame2, threshold=0.20):
+    
+    concatenated_landmarks = np.concatenate(all_landmarks, axis=0)
+    concatenated_prediction = np.concatenate(prediction, axis=0)
+    print("c1",concatenated_landmarks.shape)
+    print("c2",concatenated_prediction.shape)
+    return concatenated_landmarks, concatenated_prediction
+
+def is_similar_frame(frame1, frame2, threshold=0.05):
+    if frame1 is None:
+        return False
     distance = np.linalg.norm(frame1 - frame2)
     return distance < threshold
 
+def defineSE(arr):
+    s,e = 0, len(arr)-1
+    for i in range(len(arr)-1):
+        if arr[i] != arr[i+1]:
+            s = i + 1
+            break
+            
+    for i in range(len(arr)-1,0,-1):
+        if arr[i] != arr[i-1]:
+            e = i
+            break
+    return s,e
+    
+
 # Get the list of .npy files to load
-npy_files = ['landmarks_data1.npy',
-             'landmarks_data2.npy',
-             'landmarks_data3.npy',
-             'landmarks_data4.npy']
-concatenated_landmarks_array = load_and_concatenate_npy_files(npy_files)
+import glob
+import os
+ # load model
+model = load_model("model_final.pth")
+
+npy_folder = './temp'
+npy_files = glob.glob(os.path.join(npy_folder, '*'))
+
+concatenated_landmarks_array, concatenated_prediction_array = load_and_concatenate_npy_files(model, npy_files)
 
 frame_index = 0
 num_frames = len(concatenated_landmarks_array)
 
 image_height, image_width = 720, 1280
 
+start, end = defineSE(concatenated_prediction_array[:,0])
+print("start", start)
+print("end", end)
 last_frame_landmarks = None
-
 while frame_index < num_frames:
     image = np.ones((image_height, image_width, 3), dtype=np.uint8) * 255
     frame_landmarks = concatenated_landmarks_array[frame_index]
 
     result_image = draw_landmarks(image, frame_landmarks)
-    if result_image is None:
+    if (frame_index >= start and frame_index <= end) and concatenated_prediction_array[frame_index][0] == 0:
         frame_index += 1
         continue
+    
+    if result_image is None or is_similar_frame(last_frame_landmarks, frame_landmarks):
+        frame_index += 1
+        continue
+    
+    last_frame_landmarks = frame_landmarks
 
     cv2.imshow('Landmarks Visualization', result_image)
     frame_index += 1
 
-    if cv2.waitKey(20) & 0xFF == ord('q'):  
+    if cv2.waitKey(1) & 0xFF == ord('q'):  
         break
 
 
